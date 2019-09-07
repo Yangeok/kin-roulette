1603b450dff0a236691655dcffd3c494a3a54df3
Yangwook <wooky92@naver.com>
2019-09-07 11:29:16 +0900

    -

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo ""
+echo "--------------------"
+echo "<NAVER KIN ROULETTE>"
+echo "--------------------"
+echo ""
+echo -n "> Username: "
+read ID
+echo -n "> Password: "
+stty -echo
+read PW
+stty echo
+echo ""
+echo ""
+echo "> Configure execution file"
+echo -n "( 1: Get roulettes / 2: Use roulettes ): "
+
+read ENV
+if [ $ENV -eq  1 ]
+then
+  ID=$ID PW=$PW node getRoulette.js 
+elif [ $ENV -eq 2 ]
+then
+  ID=$ID PW=$PW node useRoulette.js 
+fi
