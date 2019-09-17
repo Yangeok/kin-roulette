a17958cf1c600ac0706168bbb116fb80e561eae5
Yangwook Jeong <wooky92@naver.com>
2019-09-17 17:40:55 +0900

    -

--- pre-start.sh
+++ pre-start.sh
@@ -1,9 +1,9 @@
 npm i -g yarn && yarn
 
 echo ""
-echo "> 모듈이 모두 설치 되었습니다."
+echo "> Modules are installed"
 echo ""
 function pause(){
   read -p "$*"
 }
-pause '> Enter를 눌러 쉘을 닫으세요.'
+pause '> Press enter key...'
